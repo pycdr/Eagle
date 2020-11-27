6e7e163fcedd1a71b0b1e1f632d6d2dd914948e7
mmt <pycdremail@gmail.com>
2020-11-27 16:38:28 +0330

    debugging

--- main.py
+++ main.py
@@ -7,19 +7,16 @@ from validators import url as is_url
 console = Console()
 parser = ArgumentParser(description = "Eagle - a download manager based on CLI")
 
-parser.add_argument("url", required = False,
+parser.add_argument("url",
 	help = "the url that file will be downloaded from")
 parser.add_argument("-o", "--output", required = False,
 	help = "the path where the file will be saved there")
 
 args = parser.parse_args()
 
-if not args.url:
-	console.log("[red]Error: please give url to download from it![/red]")
-	exit()
 if not is_url(args.url):
 	console.log("[red]Error: please give a valid url.[/red]")
-	if not url.startswith("https://") or not url.startswith("http://"):
+	if not args.url.startswith("https://") or not args.url.startswith("http://"):
 		console.print("[yellow]maybe you meant http[s]://" + url + "[/yellow]")
 	exit()
 
