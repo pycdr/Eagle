f1089ac25ddbfa4eecd6d4fe4b5a6db45ace2286
mmt <pycdremail@gmail.com>
2020-11-27 22:46:44 +0330

    debugging

--- tools/progressbar.py
+++ tools/progressbar.py
@@ -3,11 +3,12 @@ from rich.progress import Progress
 class ProgressBar:
 	def __init__(self, downloader_obj, console):
 		self.dl_obj = downloader_obj
-		self.progress = Progress(console = console, transient = True)
+		self.console = console
 	def start(self):
 		self.csize = 0
-		self.task = self.progress.add_task("[green] downloading...", total = self.dl_obj.size)
-		while not self.dl_obj.finished:
-			adv = self.dl_obj.downloaded_size - self.csize
-			self.progress.update(self.task, advance = adv)
-			self.csize = self.dl_obj.downloaded_size
+		with Progress(console = self.console, transient = True) as self.progress:
+			self.task = self.progress.add_task("[green] downloading...", total = self.dl_obj.size)
+			while not self.dl_obj.finished:
+				adv = self.dl_obj.downloaded_size - self.csize
+				self.progress.update(self.task, advance = adv)
+				self.csize = self.dl_obj.downloaded_size
